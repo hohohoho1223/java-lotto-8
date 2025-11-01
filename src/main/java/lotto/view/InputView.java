b89c08d6838cf84adb1cebd9a0a1de55f90d817a
hohohoho1223 <dnjsgh1820@naver.com>
2025-11-02 02:32:31 +0900

    feat(InputView): 사용자 당첨 번호 입력 기능 구현
    - 쉼표(,) 기준으로 숫자 분리 기능
    - 중복 검증
    - 1~45내 범위인지 검증

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -2,6 +2,10 @@ package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class InputView {
 
     public static int inputPurchaseAmount() {
@@ -26,6 +30,29 @@ public class InputView {
         }
         if (amount%1000 != 0) {
             throw new IllegalArgumentException("[ERROR] 입력금액은 1000원 단위여야 합니다!");
+        }
+    }
+
+    public static List<Integer> inputLottoNumber() {
+        System.out.println("예상되는 당첨 번호를 입력해주세요! (단, 쉼표(,)를 이용해 구분하여 입력하세요!:");
+        String input = Console.readLine();
+        List<Integer> lottoNumbers = Arrays.stream(input.split(","))
+                .map(s -> s.trim())
+                .map(InputView::parseToInt)
+                .collect(Collectors.toList());
+        validateLottoNumbers(lottoNumbers);
+        return lottoNumbers;
+    }
+
+    public static void validateLottoNumbers(List<Integer> lottoNumbers) {
+        if (lottoNumbers.size() != 6) {
+            throw new IllegalArgumentException("[ERROR] 당첨 번호는 6개여야 합니다!");
+        }
+        if (lottoNumbers.stream().distinct().count() != 6) {
+            throw new IllegalArgumentException("[ERROR] 중복되지 않은 번호여야 합니다!");
+        }
+        if (lottoNumbers.stream().anyMatch(n -> n < 1 || n > 45)) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이여야 합니다.");
         }
     }
 }
