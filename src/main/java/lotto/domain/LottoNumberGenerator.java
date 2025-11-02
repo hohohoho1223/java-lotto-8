7b3d9be4c93ca23b5da9a3c17a822ae76e51aee8
hohohoho1223 <dnjsgh1820@naver.com>
2025-11-02 15:38:18 +0900

    feat(Generator): 당첨 로또 번호 생성 기능 구현
    - 1~45내 범위 검증 기능
    - 중복 및 번호가 6개인지 검증
    - 오름차순으로 정렬하여 반환 기능

---
+++ src/main/java/lotto/domain/LottoNumberGenerator.java
@@ -0,0 +1,14 @@
+package lotto.domain;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.List;
+
+public class LottoNumberGenerator {
+    public Lotto Generator() {
+        List<Integer> randomWinNumbers = Randoms.pickUniqueNumbersInRange(1, 45, 6)
+                .stream()
+                .sorted() // 오름차순 정렬
+                .toList(); // stream()은 데이터 흐름을 나타내는 중간 처리 도구라서 리스트가 아님 -> 리스트화 필요
+        return new Lotto(randomWinNumbers); // 유효한 로또 객체로 전환
+    }
+}
