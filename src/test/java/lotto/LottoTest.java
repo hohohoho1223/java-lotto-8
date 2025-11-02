02df699112a99c308d553076ff13110a5b18da19
hohohoho1223 <dnjsgh1820@naver.com>
2025-11-02 16:39:49 +0900

    test(domain): 로또 번호에 대한 유효성 검증 로직 단위 테스트 구현
    - 실패시 명확한 원인을 알기 위해 1미만인 경우와 45초과인 경우를 나누어서 테스트를 구현함
    - 각 조건은 독립된 조건 분기임을 확인

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,5 +1,6 @@
 package lotto;
 
+import lotto.domain.Lotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -21,5 +22,19 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // TODO: 추가 기능 구현에 따른 테스트 코드 작성
+    @DisplayName("로또 번호가 1 미만일 경우 예외가 발생한다.")
+    @Test
+    void 로또_번호가_1미만일_경우_예외() {
+        assertThatThrownBy(() -> new Lotto(List.of(0, 2, 3, 4, 5, 6)))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("1부터 45");
+    }
+
+    @DisplayName("로또 번호가 45 초과일 경우 예외가 발생한다.")
+    @Test
+    void 로또_번호가_45초과일_경우_예외() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 100)))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("1부터 45");
+    }
 }
