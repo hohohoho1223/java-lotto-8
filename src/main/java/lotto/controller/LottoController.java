75f59c6baeb8eb5cc6a2e9d7bf88f5c3545647d8
hohohoho1223 <dnjsgh1820@naver.com>
2025-11-03 23:19:21 +0900

    refactor(controller): IllegalStateException 예외 처리 구조 추가
    - 기존 로직 변경 없이 구조만 추가하였기에 refactor로 분류
    - 프로그램 상태 이상 발생 시 IllegalStateException 처리 구조 추가

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -2,25 +2,52 @@ package lotto.controller;
 
 import lotto.domain.Lotto;
 import lotto.domain.LottoNumberGenerator;
+import lotto.domain.LottoResult;
 import lotto.view.InputView;
+import lotto.view.OutputView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class LottoController {
     private final LottoNumberGenerator generator = new LottoNumberGenerator();
 
-    public void run(){
-        int purchaseAmount = InputView.inputPurchaseAmount();
-        int count = calculateLottoCount(purchaseAmount);
+    public void run() {
+        try {
+            int purchaseAmount = InputView.inputPurchaseAmount();
+            int count = calculateLottoCount(purchaseAmount);
 
-        System.out.println(count+"개를 구매했습니다.");
+            List<Integer> lottoNumbers = InputView.inputLottoNumber();
+            int bonusNumber = InputView.inputBonusNumber(lottoNumbers);
+            List<Lotto> lottos = generateLottos(count);
 
-        for (int i = 0; i < count; i++) {
-            Lotto lotto = generator.Generator();
-            System.out.println(lotto.getNumbers());
+            // 예: 로또가 아예 생성되지 않은 경우
+            if (lottos.isEmpty()) {
+                throw new IllegalStateException("[ERROR] 발행된 로또가 없습니다.");
+            }
+
+            LottoResult result = new LottoResult(lottos, lottoNumbers, bonusNumber);
+            OutputView.printResult(result);
+
+        } catch (IllegalStateException e) {
+            // 프로그램 로직 상태 오류 처리
+            System.out.println(e.getMessage());
         }
     }
 
-    public int calculateLottoCount(int amount) {
+    private List<Lotto> generateLottos(int count) {
+        System.out.println(count+"개를 구매했습니다.");
+        List<Lotto> lottos = new ArrayList<>();
 
+        for (int i = 0; i < count; i++) {
+            Lotto lotto = generator.generate();
+            System.out.println(lotto.getNumbers());
+            lottos.add(lotto);
+        }
+        return lottos;
+    }
+
+    public int calculateLottoCount(int amount) {
         int lottoCount = amount/1000;
         return lottoCount;
     }
