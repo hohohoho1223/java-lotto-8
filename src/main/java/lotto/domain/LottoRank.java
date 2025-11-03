a54651d39ced9eff2feb632e225dcf4680ae33d5
hohohoho1223 <dnjsgh1820@naver.com>
2025-11-03 20:33:26 +0900

    refactor(domain): Java 네이밍 컨벤션 및 실제 메서드명 일치(=표준 일관성 유지)
    - LottoValue() 메서드명을 valueOf()로 변경하여 Java Enum 표준 메서드명과 일치시킴
    - LottoRank Enum 내부의 조건문에서 matchBonus == true → matchBonus 로 단순화

--- src/main/java/lotto/domain/LottoRank.java
+++ src/main/java/lotto/domain/LottoRank.java
@@ -23,9 +23,9 @@ public enum LottoRank {
         return prize;
     }
 
-    public static LottoRank LottoValue(int matchCount, boolean matchBonus) {
+    public static LottoRank valueOf(int matchCount, boolean matchBonus) {
         if (matchCount == 6) return FIRST;
-        if (matchCount == 5 && matchBonus == true) return SECOND;
+        if (matchCount == 5 && matchBonus) return SECOND;
         if (matchCount == 5) return THIRD;
         if (matchCount == 4) return FOURTH;
         if (matchCount == 3) return FIFTH;
